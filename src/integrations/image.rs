b8b5d1b22c55af6825ad9cb342e38df5f0846130
CodeBunny <slayer111315@gmail.com>
2022-05-26 14:37:04 +1000

    added Lerpable and IntoScalar trait implementations for Rgba<u8>

--- src/integrations/image.rs
+++ src/integrations/image.rs
@@ -3,7 +3,7 @@ use image::{RgbaImage, Rgba};
 use crate::{
     char::BunnyChar,
     font::BunnyFont,
-    traits::{color::Color, pixel_indexable::PixelIndexable, source_image::SourceImage},
+    traits::{color::Color, into_scalar::IntoScalar, lerpable::Lerpable, pixel_indexable::PixelIndexable, source_image::SourceImage},
 };
 
 pub type ImageBunnyFont = BunnyFont<RgbaImage>;
@@ -22,5 +22,25 @@ impl SourceImage for RgbaImage {
 impl PixelIndexable<Rgba<u8>> for RgbaImage {
     fn get_pixel_at(&self, x: usize, y: usize) -> &Rgba<u8> {
         self.get_pixel(x as u32, y as u32)
+    }
+}
+
+impl Lerpable for Rgba<u8> {
+    fn lerp(a: &Self, b: &Self, scalar: f32) -> Self {
+        [
+            (a.0[0] as f32 * scalar + b.0[0] as f32 * (1.0 - scalar)) as u8,
+            (a.0[1] as f32 * scalar + b.0[1] as f32 * (1.0 - scalar)) as u8,
+            (a.0[2] as f32 * scalar + b.0[2] as f32 * (1.0 - scalar)) as u8,
+            (a.0[3] as f32 * scalar + b.0[3] as f32 * (1.0 - scalar)) as u8,
+        ].into()
+    }
+}
+
+impl IntoScalar for Rgba<u8> {
+    fn into_scalar(&self) -> f32 {
+        (self.0[0] as f32 +
+        self.0[1] as f32 +
+        self.0[2] as f32 +
+        self.0[4] as f32) / 256.0
     }
 }
